740996fc9e1c6ee546a123caa40e476941826e8a
Hossein Mousavi <hossein13mousavi@gmail.com>
2023-01-08 14:24:59 +0330

    chore(global)!: remove useless files

--- two-sum/answer.js
+++ two-sum/answer.js
@@ -3,9 +3,9 @@
     https://leetcode.com/problems/two-sum
 */
 function twoSum(nums, target) {
-    var result = [];
-    for (var firstIndex = 0; firstIndex < nums.length; firstIndex++) {
-        for (var secondIndex = firstIndex + 1; secondIndex < nums.length; secondIndex++) {
+    let result = [];
+    for (let firstIndex = 0; firstIndex < nums.length; firstIndex++) {
+        for (let secondIndex = firstIndex + 1; secondIndex < nums.length; secondIndex++) {
             if (nums[firstIndex] + nums[secondIndex] === target) {
                 result = result.length ? result : [firstIndex, secondIndex];
             }
@@ -13,6 +13,7 @@ function twoSum(nums, target) {
     }
     return result;
 }
+
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
